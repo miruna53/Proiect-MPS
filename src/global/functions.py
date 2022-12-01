709c31dbcb5b8db3a41e7048e5866186e29422e6
Andrei Caraimane <caraiman.andreitimotei@gmail.com>
2022-12-01 20:28:39 +0200

    functions

--- src/global/functions.py
+++ src/global/functions.py
@@ -1,19 +1,27 @@
-def f1(t1, t2, t3, optimal_threshold):
+import math
+
+
+def f1(t1, t2, t3, optimal_threshold, f_measures):
     return (t1 + t2 + t3) / 3
 
 
-def f2(t1, t2, t3, optimal_threshold):
-    dif1 = abs(t1 - optimal_threshold)
-    dif2 = abs(t2 - optimal_threshold)
-    dif3 = abs(t3 - optimal_threshold)
+def f2(t1, t2, t3, optimal_threshold, f_measures):
+    dif1 = abs(f_measures[math.floor(t1 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif2 = abs(f_measures[math.floor(t2 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif3 = abs(f_measures[math.floor(t3 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
 
-    return min(dif1, dif2, dif3)
+    if dif1 == min(dif1, dif2, dif3):
+        return t1
+    if dif2 == min(dif1, dif2, dif3):
+        return t2
+    if dif3 == min(dif1, dif2, dif3):
+        return t3
 
 
-def f3(t1, t2, t3, optimal_threshold):
-    dif1 = abs(t1 - optimal_threshold)
-    dif2 = abs(t2 - optimal_threshold)
-    dif3 = abs(t3 - optimal_threshold)
+def f3(t1, t2, t3, optimal_threshold, f_measures):
+    dif1 = abs(f_measures[math.floor(t1 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif2 = abs(f_measures[math.floor(t2 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif3 = abs(f_measures[math.floor(t3 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
 
     if dif1 == min(dif1, dif2, dif3):
         if dif2 == min(dif2, dif3):
@@ -34,18 +42,26 @@ def f3(t1, t2, t3, optimal_threshold):
             return 0.7 * t3 + 0.2 * t2 + 0.1 * t1
 
 
-def f4(t1, t2, t3, optimal_threshold):
-    dif1 = abs(t1 - optimal_threshold)
-    dif2 = abs(t2 - optimal_threshold)
-    dif3 = abs(t3 - optimal_threshold)
+def f4(t1, t2, t3, optimal_threshold, f_measures):
+    dif1 = abs(f_measures[math.floor(t1 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif2 = abs(f_measures[math.floor(t2 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
+    dif3 = abs(f_measures[math.floor(t3 * 255)] - f_measures[math.floor(optimal_threshold * 255)])
 
     if dif1 == max(dif1, dif2, dif3):
-        return (dif2 + dif3) / 2
+        return (t2 + t3) / 2
     if dif2 == max(dif1, dif2, dif3):
-        return (dif1 + dif3) / 2
+        return (t1 + t3) / 2
     if dif3 == max(dif1, dif2, dif3):
-        return (dif2 + dif1) / 2
+        return (t2 + t1) / 2
 
 
-def f5(t1, t2, t3, optimal_threshold):
+def f5(t1, t2, t3, optimal_threshold, f_measures):
     return (t1 * t2 * t3) ** (1./3.)
+
+
+def root1(t1, t2, t3, t4, t5, optimal_threshold, f_measures):
+    return (t1 + t2 + t3 + t4 + t5) / 5
+
+
+def root2(t1, t2, t3, t4, t5, optimal_threshold, f_measures):
+    return (t1 * t2 * t3 * t4 * t5) ** (1./5.)
--- src/global/main.py
+++ src/global/main.py
@@ -1,6 +1,19 @@
 from csv_parser import *
 from file import *
+from functions import *
 
+def apply_operations(file):
+    # print(f1(file.thresholds[0], file.thresholds[1], file.thresholds[2], file.optimal_threshold, file.f_measures))
+    # print(f2(file.thresholds[3], file.thresholds[4], file.thresholds[5], file.optimal_threshold, file.f_measures))
+    # print(f3(file.thresholds[6], file.thresholds[7], file.thresholds[8], file.optimal_threshold, file.f_measures))
+    # print(f4(file.thresholds[9], file.thresholds[10], file.thresholds[11], file.optimal_threshold, file.f_measures))
+    # print(f5(file.thresholds[12], file.thresholds[13], file.thresholds[14], file.optimal_threshold, file.f_measures))
+    return root1(f1(file.thresholds[0], file.thresholds[1], file.thresholds[2], file.optimal_threshold, file.f_measures),
+                 f2(file.thresholds[3], file.thresholds[4], file.thresholds[5], file.optimal_threshold, file.f_measures),
+                 f3(file.thresholds[6], file.thresholds[7], file.thresholds[8], file.optimal_threshold, file.f_measures),
+                 f4(file.thresholds[9], file.thresholds[10], file.thresholds[11], file.optimal_threshold, file.f_measures),
+                 f5(file.thresholds[12], file.thresholds[13], file.thresholds[14], file.optimal_threshold, file.f_measures)
+                 , file.optimal_threshold, file.f_measures)
 
 def main():
     files = []
@@ -8,7 +21,10 @@ def main():
     for csv_file in get_files():
         files.append(parse_file(csv_file))
 
-    print(files)
+    print(apply_operations(files[0]))
+
+    print(files[0].f_measures[math.floor(255 * apply_operations(files[0]))])
+    print(files[0].f_measures[math.floor(255 * files[0].optimal_threshold)])
 
 
 if __name__ == "__main__":
