82fab038f1fbec31607bfacd2ad4b0a77ba9b33d
Andrei Caraimane <caraiman.andreitimotei@gmail.com>
2022-12-01 20:29:49 +0200

    functions

--- src/global/main.py
+++ src/global/main.py
@@ -2,18 +2,21 @@ from csv_parser import *
 from file import *
 from functions import *
 
+
 def apply_operations(file):
     # print(f1(file.thresholds[0], file.thresholds[1], file.thresholds[2], file.optimal_threshold, file.f_measures))
     # print(f2(file.thresholds[3], file.thresholds[4], file.thresholds[5], file.optimal_threshold, file.f_measures))
     # print(f3(file.thresholds[6], file.thresholds[7], file.thresholds[8], file.optimal_threshold, file.f_measures))
     # print(f4(file.thresholds[9], file.thresholds[10], file.thresholds[11], file.optimal_threshold, file.f_measures))
     # print(f5(file.thresholds[12], file.thresholds[13], file.thresholds[14], file.optimal_threshold, file.f_measures))
-    return root1(f1(file.thresholds[0], file.thresholds[1], file.thresholds[2], file.optimal_threshold, file.f_measures),
-                 f2(file.thresholds[3], file.thresholds[4], file.thresholds[5], file.optimal_threshold, file.f_measures),
-                 f3(file.thresholds[6], file.thresholds[7], file.thresholds[8], file.optimal_threshold, file.f_measures),
-                 f4(file.thresholds[9], file.thresholds[10], file.thresholds[11], file.optimal_threshold, file.f_measures),
-                 f5(file.thresholds[12], file.thresholds[13], file.thresholds[14], file.optimal_threshold, file.f_measures)
-                 , file.optimal_threshold, file.f_measures)
+    return root1(
+        f1(file.thresholds[0], file.thresholds[1], file.thresholds[2], file.optimal_threshold, file.f_measures),
+        f2(file.thresholds[3], file.thresholds[4], file.thresholds[5], file.optimal_threshold, file.f_measures),
+        f3(file.thresholds[6], file.thresholds[7], file.thresholds[8], file.optimal_threshold, file.f_measures),
+        f4(file.thresholds[9], file.thresholds[10], file.thresholds[11], file.optimal_threshold, file.f_measures),
+        f5(file.thresholds[12], file.thresholds[13], file.thresholds[14], file.optimal_threshold, file.f_measures)
+        , file.optimal_threshold, file.f_measures)
+
 
 def main():
     files = []
